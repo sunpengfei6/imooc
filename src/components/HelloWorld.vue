370c25c7ecf48c0e0102570557f061934aa7607a
Sunpengfei6 <415287372@qq.com>
2017-10-10 20:26:20 +0800

    添加公共页面

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,53 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+  <div>
+  <div class="top">
+    <img src="../assets/1.jpg" width="126px">
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li><a href="#">课程</a></li>
+      <li><a href="#">职业路径</a></li>
+      <li><a href="#">实战</a></li>
+      <li><a href="#">猿问</a></li>
+      <li><a href="#">手记</a></li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <input type="text">
+  </div>
+
+    <div>
+
+  </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js'
-    }
-  }
-}
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style >
+  *{
+    padding:0;
+    margin:0;;
+  }
+  .top{
+    width:100%;
+    height:72px;
+    background-color: #eee;
+  }
+  .top image{
+    margin-top: 30px;
+    width: 126px;
+    float: left;
+  }
+  .top li{
+    float: left;
+    list-style-type: none;
+    width: 100px;
+    text-align: center;
+  }
+  .top li a{
+    text-decoration: none;
+  }
 </style>
