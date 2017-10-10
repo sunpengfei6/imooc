c7243b4cff1ad48202ca8bc223c89cfd8d5c98c3
Sunpengfei6 <415287372@qq.com>
2017-10-10 21:04:13 +0800

    添加公共页面

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
   <div class="top">
     <img src="../assets/1.jpg" width="126px">
     <ul>
-      <li><a href="#">课程</a></li>
+      <li><a href="#">课程 </a></li>
       <li><a href="#">职业路径</a></li>
       <li><a href="#">实战</a></li>
       <li><a href="#">猿问</a></li>
---
+++ src/components/project/t.vue
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My first Vue app</title>
+    <script src="https://unpkg.com/vue"></script>
+</head>
+<body>
+<div id="app">
+    {{ message }}
+</div>
+
+<script>
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello Vue!'
+        }
+    })
+</script>
+</body>
+</html><template>
+
+</template>
+
+<script>
+    export default {
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
