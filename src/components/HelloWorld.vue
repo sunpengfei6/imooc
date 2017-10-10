0c87641f884434cac6a17a9cf7dec42043bf9a3d
Sunpengfei6 <415287372@qq.com>
2017-10-10 23:12:05 +0800

    添加公共页面

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,36 @@
 <template>
   <div>
+
   <div class="top">
-    <img src="../assets/1.jpg" width="126px">
-    <ul>
-      <li><a href="#">课程 </a></li>
-      <li><a href="#">职业路径</a></li>
-      <li><a href="#">实战</a></li>
-      <li><a href="#">猿问</a></li>
-      <li><a href="#">手记</a></li>
-    </ul>
-    <input type="text">
+
+    <div class="img">
+      <img src="../assets/1.jpg" width="160">
+    </div>
+
+   <div class="menu">
+     <ul>
+       <li><a href="#">课程 </a></li>
+       <li><a href="#">职业路径</a></li>
+       <li><a href="#">实战</a></li>
+       <li><a href="#">猿问</a></li>
+       <li><a href="#">手记</a></li>
+     </ul>
+   </div>
+
+    <div class="serach">
+      <a>前端入门</a>
+      <a>Java入门</a>
+    </div>
+
+    <div><a>下载APP</a></div>
+
+    <div><button>购物车</button></div>
+
+    <div><img src="../assets/2.png" width="35px"></div>
   </div>
 
-    <div>
 
-  </div>
+
   </div>
 </template>
 
@@ -32,20 +48,36 @@
   .top{
     width:100%;
     height:72px;
-    background-color: #eee;
+    background-color: #fff; 
   }
-  .top image{
-    margin-top: 30px;
-    width: 126px;
+  .img{
+    margin:10px 30px;
     float: left;
+    display: block;
   }
-  .top li{
+  .menu li{
     float: left;
     list-style-type: none;
-    width: 100px;
+    padding-left: 20px;
+    padding-right: 10px;
     text-align: center;
+    line-height: 72px;
+    display: block;
   }
-  .top li a{
+  .menu a:link,.menu a:visited{
     text-decoration: none;
+    color: #333;
+    font-size: large;
+  }
+  .menu a:hover,.menu a:active{
+    color: #000;
+  }
+  .serach a{
+    border: 1px solid;
+    border-radius: 25px;
+    width: 100px;
+    height:25px;
+    float: left;
+
   }
 </style>
