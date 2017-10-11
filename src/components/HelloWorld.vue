54a743d1f179abae23fc71ab0bb66f74a130f8cb
Sunpengfei6 <415287372@qq.com>
2017-10-11 22:49:21 +0800

    添加公共页面

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,83 +1,132 @@
 <template>
-  <div>
+    <div>
 
-  <div class="top">
+        <div class="top">
 
-    <div class="img">
-      <img src="../assets/1.jpg" width="160">
+            <div class="img">
+                <img src="../assets/1.jpg" width="160">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="#">课程 </a></li>
+                    <li><a href="#">职业路径</a></li>
+                    <li><a href="#">实战</a></li>
+                    <li><a href="#">猿问</a></li>
+                    <li><a href="#">手记</a></li>
+                </ul>
+            </div>
+
+            <div class="search">
+                <a href="#">前端入门</a>
+                <a href="#">Java入门</a>
+            </div>
+
+            <div class="download"><a>下载APP</a></div>
+
+            <div class="img1"><img src="../assets/2.png" width="35px"></div>
+
+            <div class="gou">
+                <a>购物车</a>
+            </div>
+
+
+        </div>
+
+
     </div>
-
-   <div class="menu">
-     <ul>
-       <li><a href="#">课程 </a></li>
-       <li><a href="#">职业路径</a></li>
-       <li><a href="#">实战</a></li>
-       <li><a href="#">猿问</a></li>
-       <li><a href="#">手记</a></li>
-     </ul>
-   </div>
-
-    <div class="serach">
-      <a>前端入门</a>
-      <a>Java入门</a>
-    </div>
-
-    <div><a>下载APP</a></div>
-
-    <div><button>购物车</button></div>
-
-    <div><img src="../assets/2.png" width="35px"></div>
-  </div>
-
-
-
-  </div>
 </template>
 
 <script>
 
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style >
-  *{
-    padding:0;
-    margin:0;;
-  }
-  .top{
-    width:100%;
-    height:72px;
-    background-color: #fff; 
-  }
-  .img{
-    margin:10px 30px;
-    float: left;
-    display: block;
-  }
-  .menu li{
-    float: left;
-    list-style-type: none;
-    padding-left: 20px;
-    padding-right: 10px;
-    text-align: center;
-    line-height: 72px;
-    display: block;
-  }
-  .menu a:link,.menu a:visited{
-    text-decoration: none;
-    color: #333;
-    font-size: large;
-  }
-  .menu a:hover,.menu a:active{
-    color: #000;
-  }
-  .serach a{
-    border: 1px solid;
-    border-radius: 25px;
-    width: 100px;
-    height:25px;
-    float: left;
+<style>
+    * {
+        padding: 0;
+        margin: 0;;
+    }
 
-  }
+    .top {
+        width: 100%;
+        height: 72px;
+        background-color: #fff;
+    }
+
+    .img {
+        margin: 10px 30px;
+        float: left;
+        display: block;
+    }
+
+    .menu li {
+        float: left;
+        list-style-type: none;
+        padding-left: 20px;
+        padding-right: 10px;
+        text-align: center;
+        line-height: 72px;
+        display: block;
+    }
+
+    .menu a:link, .menu a:visited {
+        text-decoration: none;
+        color: #333;
+        font-size: large;
+    }
+
+    .menu a:hover, .menu a:active {
+        color: #000;
+    }
+    .search{
+        padding-left: 100px;
+        height: 72px;
+        width:200px;
+        float: left;
+        line-height: 72px;
+    }
+    .gou{
+        padding-right: 80px;
+        height: 72px;
+        float: right;
+        line-height: 72px;
+    }
+    .search a{
+        height: 35px;
+        border: 1px solid;
+        border-radius: 35px;
+        background: #eee;
+        text-decoration: none;
+        font-size: smaller;
+        padding:5px 10px;
+        color: #666;
+        /*float: left;*/
+    }
+    .download{
+        float: left;
+        width: 150px;
+        height: 72px;
+        line-height: 72px;
+    }
+    .download a{
+        padding-left: 50px;
+        color: #333;
+
+    }
+    .gou a{
+        height: 35px;
+        border: 1px solid;
+        border-radius: 35px;
+        background: #eee;
+        text-decoration: none;
+        font-size: medium;
+        padding:10px 30px;
+        color: #666;
+    }
+    .img1{
+        padding: 20px 50px;
+        float: right;
+    }
+
 </style>
